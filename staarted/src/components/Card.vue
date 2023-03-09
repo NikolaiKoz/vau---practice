3bcbd29979498e246b8ff688fb0e33bb14c38cd2
Vallejos Jose Luis <joseluisvallejos1995@gmail.com>
2023-03-09 18:07:57 -0300

    feat: :fire: Pruebas de componentes

---
+++ staarted/src/components/Card.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2>Auto, desde el componente Card</h2>
+        <Test />
+    </div>
+</template>
+
+<script>
+import Test from './Test.vue';
+export default {
+    components: {
+        Test,
+    }
+};
+</script>
+
+<style>
+
+</style>
